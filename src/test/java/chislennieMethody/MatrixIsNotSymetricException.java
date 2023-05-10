2e289d5849f13b7cb71e74cfad7db81247e7643b
Дмитрий Кирилин <dim_dimyych@mail.ru>
2023-05-11 00:45:30 +0400

    deleteOnePackage

--- src/test/java/chislennieMethody/MatrixIsNotSymetricException.java
+++
@@ -1,7 +0,0 @@
-package chislennieMethody;
-
-public class MatrixIsNotSymetricException extends Exception {
-    public MatrixIsNotSymetricException(String message){
-        super(message);
-    }
-}
